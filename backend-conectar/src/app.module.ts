e03c4b88f6cdddabb176f84900c6f3b2ba6aef81
mrosa16 <michaelpr3003@gmail.com>
2025-06-23 02:48:12 -0300

    git commit -m "feat: add environment configuration and Docker setup"

--- backend-conectar/src/app.module.ts
+++ backend-conectar/src/app.module.ts
@@ -5,11 +5,15 @@ import { UserEntity } from './infra/database/entities/user.entity';
 import { AuthModule } from './infra/auth/auth.module';
 import { UserModule } from './infra/http/user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
